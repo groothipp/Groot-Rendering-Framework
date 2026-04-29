b6cfdae8e16436a0c89c3faaff0f3a5cfa683d4e
Matthew Hipp <hipp.matthew@icloud.com>
2026-04-28 22:08:21 -0400

    feat: shaders store thread groups

--- src/gsl/shader.cpp
+++ src/gsl/shader.cpp
@@ -2,8 +2,8 @@
 
 namespace grf {
 
-Shader::Shader(ShaderType type, const std::string& path)
-: m_type(type), m_path(path)
+Shader::Shader(ShaderType type, const std::string& path, std::array<uint32_t, 3> threadGroup)
+: m_type(type), m_path(path), m_threadGroup(threadGroup)
 {}
 
 const ShaderType& Shader::type() const {
@@ -12,6 +12,10 @@ const ShaderType& Shader::type() const {
 
 const std::string& Shader::path() const {
   return m_path;
+}
+
+const std::array<uint32_t, 3>& Shader::threadGroup() const {
+  return m_threadGroup;
 }
 
 bool Shader::valid() const {
--- tests/shaders.cpp
+++ tests/shaders.cpp
@@ -13,4 +13,16 @@ TEST_CASE("shaders: compile", "[shaders]" ) {
   CHECK( shader.valid() );
   CHECK( shader.path() == std::format("{}/shaders/vertex.gsl", GRF_TEST_DIR));
   CHECK( shader.type() == grf::ShaderType::Vertex );
+  CHECK( shader.threadGroup() == std::array<uint32_t, 3>{ 1, 1, 1 } );
+}
+
+TEST_CASE("shaders: compute thread_group is exposed on shader", "[shaders]") {
+  grf::GRF grf;
+  grf::Shader shader = grf.compileShader(grf::ShaderType::Compute,
+    std::format("{}/shaders/compute_workgroup.gsl", GRF_TEST_DIR)
+  );
+
+  CHECK( shader.valid() );
+  CHECK( shader.type() == grf::ShaderType::Compute );
+  CHECK( shader.threadGroup() == std::array<uint32_t, 3>{ 16, 16, 1 } );
 }
